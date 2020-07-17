fb294a87cb9c65979976092242406e0dbc1fb30a
AustinHaywardIT <67304281+AustinHaywardIT@users.noreply.github.com>
2020-07-17 14:36:14 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,126 +1,107 @@
-window.addEventListener("load", () => {
-  let long;
-  let lat;
-  let temperatureDescription = document.querySelector(
-    "#temperature-description"
-  );
-  let temperatureDegree = document.querySelector(".temperature-degree");
-  let locationTimezone = document.querySelector(".location-timezone");
-  let icons = document.querySelector(".icons");
-  let windSpeed = document.querySelector(".wind-speed");
-  let feelsLike = document.querySelector(".feels-like");
+let button = document.querySelector("#button");
+let searchBox = document.querySelector("#search-box");
+let city = document.querySelector(".city");
+let feelsLike = document.querySelector(".feels-like");
+let temperature = document.querySelector(".temp");
+let weatherDescription = document.querySelector(".weather");
+let windSpeed = document.querySelector(".wind");
+let icons = document.querySelector(".icons");
+searchBox.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    document.getElementById("button").click();
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        searchBox.value +
+        "&units=metric&appid=4d6bdb6aba895a9ca3db4b8734af690f"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const locationName = data.name;
+        const temperatureData = data.main.temp;
+        const feelsLikeData = data.main.feels_like;
+        const weatherDescData = data.weather[0].main;
+        const windData = data.wind.speed * 3.6;
+        const weatherIcon = data.weather[0].icon;
+        const humidex = data.main.humidity;
+        const cloudCoverage = data.clouds.all;
+        city.innerHTML = locationName + ", " + data.sys.country;
+        temperature.innerHTML = Math.round(temperatureData) + "°C";
+        feelsLike.innerHTML = "Feels like: " + Math.round(feelsLikeData) + "°C";
+        weatherDescription.innerHTML = weatherDescData;
+        windSpeed.innerHTML = "Wind: " + Math.round(windData) + "  km/h";
+        icons.innerHTML = `<img src="${weatherIcon}.png"/>`;
+        console.log(data);
 
-  //Grabbing users location//
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      longitude = position.coords.longitude;
-      latitude = position.coords.latitude;
-      const key = "4d6bdb6aba895a9ca3db4b8734af690f";
-      console.log(position);
-      let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${key}`;
+        document.getElementById("weather-container").style.visibility =
+          "visible";
+        document.getElementById("background").style.overflow = "auto";
 
-      //Storing API data into variables//
-      fetch(api)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data);
-          const { temp } = data.main;
-          const wind = data.wind.speed * 3.6;
-          const tempDescription = data.weather[0].description;
-          const feelLikeTemp = data.main.feels_like;
+        document.getElementById("weather-container").onclick = function () {
+          humidity(), clouds(), sunSetRise(), infoDots(), infoDots2();
+        };
 
-          //Storing further API data for variables to use in functions below//
-          temperatureDegree.textContent = Math.floor(temp) + " C";
-          locationTimezone.textContent = data.name + ", " + data.sys.country;
-          temperatureDescription.textContent = tempDescription;
-          windSpeed.textContent = `Wind: ${Math.floor(wind)} km/h`;
-          feelsLike.textContent = `Feels like: ${Math.floor(feelLikeTemp)} C`;
-
-          //When user clicks on weather info, further data is displayed//
-          document.getElementById("temperature").onclick = function () {
-            humidity(), clouds(), sunSetRise(), infoDots(), infoDots2();
-          };
-
-          function humidity() {
-            if (temperatureDescription.textContent === tempDescription) {
-              temperatureDescription.textContent = `Humidex: ${data.main.humidity}%`;
-            } else {
-              temperatureDescription.textContent = tempDescription;
-            }
+        function humidity() {
+          if (
+            feelsLike.innerHTML ===
+            "Feels like: " + Math.round(feelsLikeData) + "°C"
+          ) {
+            feelsLike.innerHTML = `Humidex: ${humidex}%`;
+          } else {
+            feelsLike.innerHTML =
+              "Feels like: " + Math.round(feelsLikeData) + "°C";
           }
+        }
 
-          function clouds() {
-            if (
-              feelsLike.textContent ===
-              `Feels like: ${Math.floor(feelLikeTemp)} C`
-            ) {
-              feelsLike.textContent = `Cloud Coverage: ${data.clouds.all}%`;
-            } else {
-              feelsLike.textContent = `Feels like: ${Math.floor(
-                feelLikeTemp
-              )} C`;
-            }
+        function clouds() {
+          if (weatherDescription.innerHTML === weatherDescData) {
+            weatherDescription.innerHTML = `<div class="weather" style="font-size: 30px";>Cloud Coverage: ${cloudCoverage}%</div>`;
+          } else {
+            weatherDescription.innerHTML = weatherDescData;
           }
+        }
 
-          //formats unix into local sunset/sunrise time//
-          function sunSetRise() {
-            if (windSpeed.textContent === `Wind: ${Math.floor(wind)} km/h`) {
-              windSpeed.textContent =
-                "Sunrise: " +
-                formattedTime +
-                "AM " +
-                "|" +
-                " Sunset: " +
-                formattedTime2 +
-                "PM";
-            } else {
-              windSpeed.textContent = `Wind: ${Math.floor(wind)} km/h`;
-            }
+        function sunSetRise() {
+          if (
+            windSpeed.innerHTML ===
+            "Wind: " + Math.round(windData) + "  km/h"
+          ) {
+            windSpeed.innerHTML = `<div class="wind" style="font-size: 20px;">
+          Sunrise: ${formattedTime}AM | Sunset: ${formattedTime2} PM</div>`;
+          } else {
+            windSpeed.textContent = "Wind: " + Math.round(windData) + "  km/h";
           }
+        }
 
-          //converting unix to local sunrise time//
-          const unixTime = data.sys.sunrise;
-          const date = new Date(unixTime * 1000);
-          const hours = date.getHours();
-          const minutes = "0" + date.getMinutes();
-          const formattedTime = hours + ":" + minutes.substr(-2);
-
-          console.log(formattedTime);
-
-          const unixTime2 = data.sys.sunset;
-          const date2 = new Date(unixTime2 * 1000);
-          const hours2 = date2.getHours() - 12;
-          const minutes2 = "0" + date2.getMinutes();
-          const formattedTime2 = hours2 + ":" + minutes2.substr(-2);
-
-          console.log(formattedTime2);
-
-          //Changes icon in location container and background when live weather changes//
-          icons.innerHTML = `<img src="icons/${data.weather[0].icon}.png"/>`;
-          document.getElementById(
-            "background"
-          ).style.backgroundImage = `url(background/${data.weather[0].main}.jpg)`;
-
-          function infoDots() {
-            if (temperatureDescription.textContent === tempDescription) {
-              document.getElementById("dot1").style.backgroundColor = "black";
-            } else {
-              document.getElementById("dot1").style.backgroundColor =
-                "rgb(211, 211, 211)";
-            }
+        function infoDots() {
+          if (weatherDescription.innerHTML === weatherDescData) {
+            document.getElementById("dot1").style.backgroundColor = "black";
+          } else {
+            document.getElementById("dot1").style.backgroundColor =
+              "rgb(211, 211, 211)";
           }
+        }
 
-          function infoDots2() {
-            if (temperatureDescription.textContent === tempDescription) {
-              document.getElementById("dot2").style.backgroundColor =
-                "rgb(211, 211, 211)";
-            } else {
-              document.getElementById("dot2").style.backgroundColor = "black";
-            }
+        function infoDots2() {
+          if (weatherDescription.innerHTML === weatherDescData) {
+            document.getElementById("dot2").style.backgroundColor =
+              "rgb(211, 211, 211)";
+          } else {
+            document.getElementById("dot2").style.backgroundColor = "black";
           }
-        });
-    });
+        }
+
+        //converting unix to local sunrise time//
+        const unixTime = data.sys.sunrise;
+        const date = new Date(unixTime * 1000);
+        const hours = date.getHours();
+        const minutes = "0" + date.getMinutes();
+        const formattedTime = hours + ":" + minutes.substr(-2);
+
+        const unixTime2 = data.sys.sunset;
+        const date2 = new Date(unixTime2 * 1000);
+        const hours2 = date2.getHours() - 12;
+        const minutes2 = "0" + date2.getMinutes();
+        const formattedTime2 = hours2 + ":" + minutes2.substr(-2);
+      });
   }
 });
